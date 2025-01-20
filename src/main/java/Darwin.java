d1ccef81c66a5ed135d8c5a835247ef004ba6ecb
m0destly <e1121889@u.nus.edu>
2025-01-20 16:52:33 +0800

    Added mark, unmark functions and Task class

--- src/main/java/Darwin.java
+++ src/main/java/Darwin.java
@@ -4,24 +4,41 @@ import java.util.ArrayList;
 public class Darwin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         String line = "____________________________________________________________";
         System.out.println(line + "\nHello, I'm Darwin!\nWhat can I do for you?\n" + line);
         while (true) {
-            String input = scanner.nextLine();
+            String command = scanner.next();
             System.out.println(line);
-            if (input.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 scanner.close();
                 break;
-            } else if (input.equals("list")) {
+            } else if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= tasks.size(); i++) {
-                    System.out.println(i + ". " + tasks.get(i - 1));
+                    Task task = tasks.get(i - 1);
+                    System.out.println(i + ".[" + task.getStatusIcon() + "] " + task.getDescription());
                 }
+            } else if (command.equals("mark") && scanner.hasNextInt()) {
+                int taskNumber = scanner.nextInt() - 1;
+                Task task = tasks.get(taskNumber);
+                task.mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
+
+            } else if (command.equals("unmark") && scanner.hasNextInt()) {
+                int taskNumber = scanner.nextInt() - 1;
+                Task task = tasks.get(taskNumber);
+                task.unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
             } else {
-                tasks.add(input);
-                System.out.println("added: " + input);
+                String task = scanner.nextLine();
+                command += task;
+                tasks.add(new Task(command));
+                System.out.println("added: " + command);
             }
             System.out.println(line);
         }
