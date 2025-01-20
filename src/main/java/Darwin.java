4b1e0ec8edf4c2ba8bb12ea8319b46d0101fb7cb
m0destly <e1121889@u.nus.edu>
2025-01-20 12:15:34 +0800

    Add user input, echo commands

--- src/main/java/Darwin.java
+++ src/main/java/Darwin.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Darwin {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
-        System.out.println(line);
-        System.out.println("Hello, I'm Darwin!\nWhat can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(line + "\n" + "Hello, I'm Darwin!\nWhat can I do for you?" + "\n" + line);
+        while (true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(line + "\n" + "Bye. Hope to see you again soon!" + "\n" + line);
+                scanner.close();
+                break;
+            } else {
+                System.out.println(line + "\n" + command + "\n" + line);
+            }
+        }
     }
 }
